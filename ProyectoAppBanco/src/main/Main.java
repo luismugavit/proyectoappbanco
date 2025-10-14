0c88ac2e4f792f58a8eb131461d801ca28cec710
ander.l.g <ander.l.g@opendeusto.es>
2025-10-14 23:19:52 +0200

    Añade datos de prueba para cuentas

--- ProyectoAppBanco/src/main/Main.java
+++ ProyectoAppBanco/src/main/Main.java
@@ -20,14 +20,25 @@ public class Main {
 				new Cliente(0, "A", "A", "A", "B", new ArrayList<Cuenta>()),
 				new Cliente(1, "A", "A", "A", "B", new ArrayList<Cuenta>()),
 				new Cliente(2, "A", "A", "A", "B", new ArrayList<Cuenta>()),
-				new Cliente(4, "A", "A", "A", "B", new ArrayList<Cuenta>()),
+				new Cliente(3, "A", "A", "A", "B", new ArrayList<Cuenta>()),
+				
 		};
 		
 		for(Cliente c : clientes) {
 			
 			listaClientes.add(c);
 		}
-	
+		
+		Cuenta [] cuentas = {
+				new Cuenta("ES01", (float) 1500.50, clientes[0]),
+				new Cuenta("ES02", (float) 2435.45, clientes[1]),
+				new Cuenta("ES03", (float) 956.32, clientes[2]),
+				new Cuenta("ES04", (float) 10000.93, clientes[3]),
+		};
+		
+		for (Cuenta cuenta : cuentas) {
+			listaCuentas.add(cuenta);
+		}
 		
 		System.out.println(listaClientes.size());
 		InterfazPrueba ventana = new InterfazPrueba();
