7f4f40b7ff0587b652959961f58afdb17ee6155e
Luis Muga Vitores <luis.muga@opendeusto.es>
2025-10-29 17:35:56 +0100

    Organización del codigo y arreglos menores.

--- ProyectoAppBanco/src/gui/InterfazPrueba.java
+++ ProyectoAppBanco/src/gui/InterfazPrueba.java
@@ -4,8 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagLayout;
+
 import java.awt.GridLayout;
+import java.awt.ScrollPane;
+import java.awt.Scrollbar;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -15,8 +17,9 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
+
 import javax.swing.JPanel;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
@@ -31,11 +34,13 @@ public class InterfazPrueba extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Cliente> listaClientes;
-	private ModelTablaClientes modeloTabla;
-	private JTable tablaClientes;
+	private ModelTablaClientes modeloTabla; 
+	private JTable tablaClientes; 	//Tabla que muestra todos los clientes del banco ID/Nombre/Saldo_Total
+	private JPanel panelCont;		//Panel contenedor 
+	private CardLayout card;		//CardLayout permite alternar entre Paneles como si fuesen pestañas sin necesidad de abrir ventanas nuevas.
+	private JScrollPane scroller;
 	
 	
-	private JPanel panelCont;
 	public InterfazPrueba(ArrayList<Cliente> listaClientes, ArrayList<Cuenta> listaCuenta){
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -44,61 +49,54 @@ public class InterfazPrueba extends JFrame{
 		setLocationRelativeTo(null);
 		this.listaClientes = listaClientes;
 		
-		crearTablaClientes(listaClientes);
-		CardLayout card = new CardLayout();
+		//iniciar CardLayout
+		card = new CardLayout();
 		iniciarCardLayout(card);
 		
 		//Barra Menu y Opciones
 		JMenuBar menuBarra = new JMenuBar();
 		setJMenuBar(menuBarra);
-		//Opciones relativas a los clientes del banco
-		JMenu menuClientes = new JMenu("Clientes");
-		JMenuItem itemMain = new JMenuItem("Main");
-		
-		menuBarra.add(menuClientes);
-		menuBarra.add(itemMain);
-		JMenuItem itemVerClientes = new JMenuItem("Tabla Clientes");
-		JMenuItem itemCrearCliente = new JMenuItem("Crear cliente");
-		JMenuItem itemOpcionesCliente = new JMenuItem("Opciones");
-		menuClientes.add(itemVerClientes);
-		menuClientes.add(itemCrearCliente);
-		menuClientes.add(itemOpcionesCliente);
-		itemVerClientes.addActionListener(e -> {
-			
-			card.show(panelCont, "tablaClientes");
-		});
-		
-		itemMain.addActionListener(e -> card.show(panelCont, "inicio"));
-		itemCrearCliente.addActionListener(e -> card.show(panelCont, "crearCliente"));
-		
-		//
+		crearOpcionesMenu(menuBarra);
 		
 		setVisible(true);
+	}
+	
+	public void crearOpcionesMenu(JMenuBar menuBarra) {
+		//Opciones relativas a los clientes del banco
+			JMenu menuClientes = new JMenu("Clientes");
+			JMenuItem itemMain = new JMenuItem("Main");
+			
+			menuBarra.add(menuClientes);
+			menuBarra.add(itemMain);
+			JMenuItem itemVerClientes = new JMenuItem("Tabla Clientes");
+			JMenuItem itemCrearCliente = new JMenuItem("Crear cliente");
+			JMenuItem itemOpcionesCliente = new JMenuItem("Opciones");
+			menuClientes.add(itemVerClientes);
+			menuClientes.add(itemCrearCliente);
+			menuClientes.add(itemOpcionesCliente);
+			
+			//Al clickar en el boton correspondiente a una pestaña esta se abre con card.show( panelCont, "identificador")
+			itemVerClientes.addActionListener(e -> card.show(panelCont, "tablaClientes"));
+			itemMain.addActionListener(e -> card.show(panelCont, "inicio"));
+			itemCrearCliente.addActionListener(e -> card.show(panelCont, "crearCliente"));
+			
+			//
 	}
 	
 	public void crearTablaClientes(ArrayList<Cliente> listaClientes) {
 		
 		modeloTabla = new ModelTablaClientes(listaClientes);
 		tablaClientes = new JTable(modeloTabla);
-		tablaClientes.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+
+		scroller = new JScrollPane(tablaClientes);
+
 		
 	}
 	
-	public void iniciarCardLayout(CardLayout card) {
+	public JPanel tabCrearCliente() {
 		
-		panelCont = new JPanel(card);
-		//Panel Principal 
-		JPanel main = new JPanel();
-		main.add(new JLabel("Banco-Main"));
-		
-		//Panel de la tabla de clientes
-		
-		JPanel clientesPanel = new JPanel();
-		JScrollPane scroller = new JScrollPane(tablaClientes);
-		clientesPanel.add(scroller, BorderLayout.CENTER);
-		
-		//Panel de creación de clientes
 		JPanel addClientePanel = new JPanel(new BorderLayout());
+		//Contenido del Panel
 		addClientePanel.add(new JLabel("Add cliente", JLabel.CENTER), BorderLayout.NORTH);
 		JPanel camposTextoPanel = new JPanel(new GridLayout(4, 2, 1,60));
 		JTextField campoNombre = new JTextField();
@@ -117,9 +115,6 @@ public class InterfazPrueba extends JFrame{
 		
 		addClientePanel.add(camposTextoPanel);
 		camposTextoPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		
-	
-		
 		
 		JPanel botones = new JPanel();
 		
@@ -142,18 +137,63 @@ public class InterfazPrueba extends JFrame{
 		});
 		
 		
-	
 		
+		return addClientePanel;
+	}
+	
+	public JPanel crearMainPanel() {
+		JPanel main = new JPanel();
+		main.add(new JLabel("Banco-Main"));
+		return main;
+	}
+	
+	public JPanel tabTablaClientes() {
+		
+		JPanel panelTablaClientes = new JPanel(new BorderLayout());
+		
+		crearTablaClientes(listaClientes);
+		JPanel panelBotones = new JPanel();
+		JButton botonAddCliente = new JButton("Añadir Cliente");
+		
+		botonAddCliente.addActionListener(e -> card.show(panelCont, "crearCliente") );
+		panelBotones.add(botonAddCliente);
+		
+		
+
+		panelTablaClientes.add(scroller);
+		panelTablaClientes.add(panelBotones, BorderLayout.WEST);
+		
+		
+		
+		return panelTablaClientes;
+	}
+	
+	
+	public void iniciarCardLayout(CardLayout card) {
+		
+		panelCont = new JPanel(card);
+		
+		//Panel Principal 
+		
+		JPanel main = crearMainPanel();
+		
+		
+		//Panel de la tabla de clientes
+		
+		JPanel panelTablaClientes = tabTablaClientes();
+		
+			
+		//Panel de creación de clientes
+		
+		JPanel addClientePanel = tabCrearCliente();
+		
+	
+		//Añadir paneles al panel contenedor
 		panelCont.add(main, "inicio");
-		panelCont.add(clientesPanel, "tablaClientes");
+		panelCont.add(panelTablaClientes, "tablaClientes");
 		panelCont.add(addClientePanel, "crearCliente");
 		card.show(panelCont, "inicio");
 		
 		add(panelCont);
 	}
-	
-	
-	
-	
-	
 }
