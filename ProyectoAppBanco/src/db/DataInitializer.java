3ed2a7758a0487ec7693ef953ea921ebd19e128d
aitor.p.m <aitor.p.m@opendeusto.es>
2025-12-03 11:16:10 +0100

    Arreglado error al hacer merge en el dataInitializer

--- ProyectoAppBanco/src/db/DataInitializer.java
+++ ProyectoAppBanco/src/db/DataInitializer.java
@@ -1,5 +1,6 @@
 package db;
 
+
 import java.util.ArrayList;
 
 import domain.Cliente;
@@ -7,33 +8,29 @@ import domain.Cuenta;
 import domain.Prestamo;
 
 public class DataInitializer {
-	
+
 	GestorBDInitializer gestor = new GestorBDInitializer();
 	public void InicializarBD() {
-	
+
 		gestor.crearTablas();
-	
+
 		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-<<<<<<< HEAD
 		clientes.add(new Cliente("A", "A", "A", "B"));
+		clientes.add(new Cliente("B", "C", "A", "B"));
+		clientes.add(new Cliente("C", "A", "A", "B"));
 		clientes.add(new Cliente("B", "C", "A", "A"));
 		clientes.add(new Cliente("C", "A", "A", "D"));
-=======
-		clientes.add(new Cliente("A", "A", "A", "A"));
-		clientes.add(new Cliente("B", "C", "A", "D"));
-		clientes.add(new Cliente("C", "A", "A", "B"));
->>>>>>> branch 'main' of https://github.com/luismugavit/ProyectoAppBanco.git
 		clientes.add(new Cliente("D", "A", "B", "C"));
-		
+
 		for (Cliente cliente : clientes) {
 		    gestor.insertCliente(cliente);
 		        }
-		
+
 		Cuenta c1 = new Cuenta("A", 500, null);
 		gestor.insertCuenta(c1, clientes.get(0));
 		Cuenta c2 = new Cuenta("B", 700, null);
 		gestor.insertCuenta(c2, clientes.get(1));
-		
+
 		Prestamo p1 = new Prestamo(null, 1000, 10, 2);
 		gestor.insertPrestamo(p1, clientes.get(0));
 		Prestamo p2 = new Prestamo(null, 500, 20, 1);
